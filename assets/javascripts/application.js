fdfe968d0bacdf2b727fbd36ee5e03128d51b161
nslyc <273217471@qq.com>
2018-01-29 17:18:40 +0800

    响应式重写

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,6 +4,5 @@ import '../stylesheets/application.scss'; // 样式入口
 import './almanac.js'
 // import javascript
 import './main.js';
-import './index.js'; // js 入口
 
 // console.log('loaded application.js...');
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,9 +1,34 @@
 $(function () {
     // 检测屏幕宽度，绑定事件
-    if (document.body.clientWidth <= 750) {
+    if (document.body.clientWidth <= 770) {
+        reset();
         // 右侧按钮点击事件
         $('.nav-menu').on('click', function (e) {
             toggle();
+        })
+    } else if (document.body.clientWidth > 770 && document.body.clientWidth <= 1336) {
+        // 屏幕滚动事件
+        $(document).on('scroll', function (e) {
+            let top = $(document).scrollTop();
+            if (top >= 500) {
+                $('div.rope .top').fadeIn();
+            } else {
+                $('div.rope .top').fadeOut();
+            }
+        })
+    } else {
+        // 屏幕滚动事件
+        $(document).on('scroll', function (e) {
+            let top = $(document).scrollTop();
+            if (top >= 200) {
+                $('div.rope').css({
+                    height: top + 200
+                })
+            } else {
+                $('div.rope').css({
+                    height: 200
+                })
+            }
         })
     }
     // 导航栏列表点击事件
@@ -11,19 +36,8 @@ $(function () {
         $('.navs li').removeClass('active');
         $(this).addClass('active');
     })
-    // 屏幕滚动事件
-    $(document).on('scroll', function (e) {
-        let top = $(document).scrollTop();
-        if (top >= 200) {
-            $('div.rope').css({
-                height: top + 200
-            })
-        } else {
-            $('div.rope').css({
-                height: 200
-            })
-        }
-    })
+    
+    
     // 回到顶部事件
     $('div.rope>.top').on('click', function (e) {
         $('html,body').animate({
@@ -46,7 +60,22 @@ $(function () {
             console.log('search');
         }
     })
+    getTime();
+    setInterval(getTime, 6000)
 })
+// 时钟
+function getTime() {
+    var nowDate = new Date();
+    var h = nowDate.getHours(); //获取当前小时数(0-23)
+    h = h <= 12 ? h : (h - 12);
+    var m = nowDate.getMinutes(); //获取当前分钟数(0-59)
+    $('.clock .minute').css({
+        transform: 'rotateZ(' + (m - 10) * 6 + 'deg) translate(1px,0)'
+    })
+    $('.clock .hour').css({
+        transform: 'rotateZ(' + ((h + m / 60) * 30 + 60) + 'deg)'
+    })
+}
 
 function toggle() {
     $('.navs').toggleClass('collapse');
@@ -68,4 +97,41 @@ function toggle() {
             })
         }
     }
+}
+
+function reset() {
+    var dpr, rem, scale;
+    var docEl = document.documentElement;
+    var fontEl = document.createElement('style');
+    var metaEl = document.querySelector('meta[name="viewport"]');
+
+    dpr = window.devicePixelRatio || 1;
+    rem = docEl.clientWidth * dpr / 20;
+    scale = 1 / dpr;
+
+
+    // 设置viewport，进行缩放，达到高清效果
+    metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+    // metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+
+    // 设置data-dpr属性，留作的css hack之用
+    docEl.setAttribute('data-dpr', dpr);
+
+    // 动态写入样式
+    docEl.firstElementChild.appendChild(fontEl);
+    fontEl.innerHTML = 'html{font-size:' + rem + 'px!important;}';
+
+    // 给js调用的，某一dpr下rem和px之间的转换函数
+    window.rem2px = function (v) {
+        v = parseFloat(v);
+        return v * rem;
+    };
+    window.px2rem = function (v) {
+        v = parseFloat(v);
+        return v / rem;
+    };
+
+    window.dpr = dpr;
+    window.rem = rem;
+
 }
